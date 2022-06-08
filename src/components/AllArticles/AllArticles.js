22834ffa833db8a010bba979f34c0f4dbeb4e197
Casey Halstead <catherineclosee@gmail.com>
2022-06-07 22:02:45 -0500

    Add styling to allarticles container

---
+++ src/components/AllArticles/AllArticles.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ArticleCard from '../ArticleCard/ArticleCard';
+
+const AllArticles = ({ articles }) => {
+  let counter = 0
+  const articlesData = articles.map( article => {
+    counter += 1
+    return <ArticleCard {...article} key={counter}/>
+  })
+  
+  return (
+    <div className='all-articles-container'>
+      {articlesData}
+    </div>
+  )
+}
+
+export default AllArticles;
