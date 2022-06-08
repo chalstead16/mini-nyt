c476bbe14ba5db36235bdee5be81bfdacb256cff
Casey Halstead <catherineclosee@gmail.com>
2022-06-07 19:04:22 -0500

    refactor api call

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,18 @@
 import React, { useState, useEffect } from 'react';
+import getArticles from '../../apiCalls';
 import './App.css';
 
 const App = () => {
   const [articles, setArticles] = useState([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    getArticles()
+    .then(data => setArticles(data.results))
+    .catch(err => {
+      setError('Hmm, something went wrong...')
+    })
+  }, [])
 
   return (
     <div className="App">
