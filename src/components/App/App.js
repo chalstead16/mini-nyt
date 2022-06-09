cce19eceba6c31b16b888a8fdbaacc774993879d
Casey Halstead <catherineclosee@gmail.com>
2022-06-08 22:17:05 -0500

    Add filter method and update state to include search

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from 'react';
 import getArticles from '../../apiCalls';
 import Header from '../Header/Header';
 import AllArticles from '../AllArticles/AllArticles';
+import Form from '../Form/Form';
 import './App.css';
 
 const App = () => {
   const [articles, setArticles] = useState([]);
   const [error, setError] = useState('');
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     getArticles()
@@ -16,9 +18,12 @@ const App = () => {
     })
   }, [])
 
+  const filteredArticles = articles.filter(article => article.title.toLowerCase().includes(search) || article.title.includes(search));
+
   return (
     <div className="App">
       <Header />
+      <Form search={ search } setSearch={ setSearch }/>
       <AllArticles articles={ articles }/>
     </div>
   );
