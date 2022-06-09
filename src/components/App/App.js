8867139cf8d2a345609ec03c3c03ddb21f902897
Casey Halstead <catherineclosee@gmail.com>
2022-06-09 09:06:56 -0500

    Add modal state, logic, and return in App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import getArticles from '../../apiCalls';
 import Header from '../Header/Header';
-import AllArticles from '../AllArticles/AllArticles';
 import Form from '../Form/Form';
+import AllArticles from '../AllArticles/AllArticles';
+import SingleArticle from '../SingleArticle/SingleArticle';
 import './App.css';
 
 const App = () => {
   const [articles, setArticles] = useState([]);
   const [error, setError] = useState('');
   const [search, setSearch] = useState('');
+  const [showModal, setShowModal] = useState(false);
+  const [selectArticle, setSelectArticle] = useState(null);
 
   useEffect(() => {
     getArticles()
@@ -20,11 +23,22 @@ const App = () => {
 
   const filteredArticles = articles.filter(article => article.title.toLowerCase().includes(search.toLowerCase()));
 
+  const openModal = (article) => {
+    setSelectArticle(article)
+    setShowModal(prev => !prev)
+  }
+
    return (
     <div className="App">
       <Header />
       <Form search={ search } setSearch={ setSearch }/>
-      <AllArticles articles={ filteredArticles }/>
+      <AllArticles articles={ filteredArticles } openModal={openModal}/>
+      {showModal && <SingleArticle 
+        showModal={showModal} 
+        setShowModal={setShowModal}
+        selectArticle={selectArticle}
+      />
+      }
     </div>
   );
 }
