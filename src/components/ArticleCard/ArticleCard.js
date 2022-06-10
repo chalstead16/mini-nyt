40f673cf8eab8cde86b95c653092616e8a7d4df7
Casey Halstead <catherineclosee@gmail.com>
2022-06-10 11:58:13 -0500

    Refactor for router, update div and id in props

--- src/components/ArticleCard/ArticleCard.js
+++ src/components/ArticleCard/ArticleCard.js
@@ -1,17 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import './ArticleCard.css';
 
-const ArticleCard = ({ article, title, byline, abstract, multimedia, openModal }) => {
+const ArticleCard = ({ article, title, byline, abstract, multimedia, openModal, id }) => {
   let image = multimedia ? <img src={multimedia[2].url} alt={multimedia[2].caption} /> : null
   
   return (
-    <div className='article-card'
-    onClick={() => {openModal(article)}}>
+    <Link to={`/${id}`} style={{color:"#000000", textDecoration:"none"}} className='article-card' onClick={() => {openModal(article)}}>
       {image}
       <h2>{title}</h2>
       <h3>{byline}</h3>
       <p>{abstract}</p>
-    </div>
+    </Link>
   )
 }
 
