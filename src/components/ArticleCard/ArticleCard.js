6940c47c1d7d69eaa68b59cb3e696ab0a8a96193
Casey Halstead <catherineclosee@gmail.com>
2022-06-13 00:35:52 -0500

    Refactor link for route error handling

--- src/components/ArticleCard/ArticleCard.js
+++ src/components/ArticleCard/ArticleCard.js
@@ -6,7 +6,7 @@ const ArticleCard = ({ article, title, byline, abstract, multimedia, openModal, 
   let image = multimedia ? <img src={multimedia[2].url} alt={multimedia[2].caption} /> : null
   
   return (
-    <Link to={`/${id}`} style={{color:"#000000", textDecoration:"none"}} className='article-card' onClick={() => {openModal(article)}}>
+    <Link to={`/article${id}`} style={{color:"#000000", textDecoration:"none"}} className='article-card' onClick={() => {openModal(article)}}>
       {image}
       <h2>{title}</h2>
       <h3>{byline}</h3>
